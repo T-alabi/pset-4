526215c912b37129175202ae6e237adde1ad53a1
T-alabi <talabi@ucvts.org>
2020-09-30 10:09:28 -0400

    Ex 1 and 2 corrected and done

--- src/Exercise1.java
+++ src/Exercise1.java
@@ -1,19 +1,23 @@
 import java.util.Scanner;
 
-public class exercise1 {
+public class Exercise1 {
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
 
         //input values
-        System.out.print("Temperature:");
+        System.out.print("Temperature : ");
         double temp = input.nextDouble();
-        System.out.print("Wind speed :");
+        System.out.print("Wind speed  : ");
         double windSpeed = input.nextDouble();
 
         //math
         double calcWindChill = 35.74 + (0.6215 * temp) + ((0.4275 * temp) - 35.75)*Math.pow(windSpeed, 0.16);
 
-        System.out.println("");
-        System.out.printf("Wind Chill : %,.2f" ,  calcWindChill);
+        System.out.print("\n");
+        System.out.printf("Wind Chill  : %,.2f" ,  calcWindChill);
+
+        input.close();
+
+
     }
 }
---
+++ src/Exercise2.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Exercise2 {
+    public static void main(String[] args){
+        Scanner in =  new Scanner(System.in);
+
+        System.out.print("Inches : ");
+        int inches = in.nextInt();
+
+        int miles = inches / 63360;
+        int feet = (inches - (miles * 63360))/12;
+        int leftOverInches = (inches - ((miles * 63360) + (feet * 12)));
+
+        System.out.print("\n");
+        System.out.println("Miles  : " + miles);
+        System.out.println("Feet   : " + feet);
+        System.out.println("Inches : " + leftOverInches);
+    }
+}
